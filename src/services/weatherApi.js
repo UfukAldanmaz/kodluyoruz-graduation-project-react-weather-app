56e62ab63ecaa0acb72bed5fa87955c280df556d
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-07 12:16:14 +0300

    Update weatherApi.js

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { getTodayEpoch } from '../utils/datetimeUtils'
 
 const API_KEY = "4ec48ffb1e2151b439eda4d3d1526d93";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeatherData = async (location) => {
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&dt=1643803200&appid=${API_KEY}&units=metric`)
+    const { data } = await axios.get(`${BASE_URL}?q=${location}&dt=${getTodayEpoch()}&appid=${API_KEY}&units=metric`)
     return data;
-} 
+}
