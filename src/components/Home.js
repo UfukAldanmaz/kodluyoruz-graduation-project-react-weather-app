c5ac8589c306be2b2033c404f6d3cf7758941589
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-07 12:15:42 +0300

    add function for recent searches

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,56 +1,42 @@
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { storeLocation, getLocations } from "../storage/cityStore";
 import { getWeatherData } from "../services/weatherApi";
-import PlusCircle from '../assets/images/plus-circle-svgrepo-com.svg';
+import CurrentWeather from "./CurrentWeather";
 
 const Home = () => {
-    const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState(null);
     const [location, setLocation] = useState('');
     const [previousSearches, setPreviousSearches] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-    const navigate = useNavigate();
+    const inputRef = useRef(null)
+    const [isLoading, setIsLoading] = useState(false);
+    const [removedCity, setRemovedCity] = useState(null);
 
+    // Handle form submit to fetch weather data from API
     const handleSearchSubmit = async (e) => {
         e.preventDefault()
         try {
+            setIsLoading(true);
             const data = await getWeatherData(location);
-            storeLocation(location);
             setWeatherData(data);
-            setLoaded(true);
-            console.log('weatherData:', data);
+            setIsLoading(false);
         } catch (error) {
             console.log(error);
         }
     }
 
-
-    useEffect(() => {
-        console.log("ufuk")
-        loadPreviousSearches();
-
-        return () => {
-            setPreviousSearches([]);
-        }
-    }, [loaded])
-
+    // Load recent searches
     const loadPreviousSearches = () => {
         const fetchData = async (location) => {
             const data = await getWeatherData(location);
-            setPreviousSearches(old => [...old, data]);
+            setPreviousSearches(old => [...old, data]); console.log('fail', data);
         }
 
-        const locations = getLocations();
 
-        locations.forEach(l => {
-
-            console.log('ugur', l, location)
-            if (l === location) {
-
-                return;
-            }
+        // we read localStorage to get recently searched cities
+        // iterate them and fetch current weather data for all recently searched cities. 
+        getLocations().forEach(recentLocation => {
             try {
-                fetchData(l);
+                fetchData(recentLocation);
 
             } catch (error) {
                 // TODO: notification
@@ -59,17 +45,87 @@ const Home = () => {
     }
 
     useEffect(() => {
-        if (previousSearches.length === 3) {
-            setLocation("");
+        // refresh previous search
+
+        // if we have no weatherData, then do nothing.
+        if (!weatherData) {
+            return
         }
 
-    }, [previousSearches])
+        // copy previousSearches state into local variable: 'refreshed'
+        // we will manipulate this local variable later.
+        const refreshed = [...previousSearches];
+
+        if (refreshed.length < 3) {
+            // If user has not searched more than 3 cities yet
+            // then just push the latest weatherData to previousSearches state.
+            setPreviousSearches(old => [weatherData, ...old])
+            return;
+        }
+
+
+        // removedCity is the city name that lastly removed from the localStorage
+        // we remove older cities when localStorage reaches 3 items.
+
+        if (removedCity === null) {
+            return;
+        }
+
+        const index = refreshed.findIndex(item => item.name.toUpperCase() === removedCity.toUpperCase());
+        if (index > -1) {
+            // if lastly removedCity is in the previously searched weather data list,
+            // then simply remove it from the list
+            // then push the last weather data to at the very beginning of the previously searched list.
+            refreshed.splice(index, 1);
+            refreshed.unshift(weatherData);
+
+            // we set previousSearches state with the local variable: refreshed. 
+            // because this is the best way to manipulate array of object states.
+            setPreviousSearches(refreshed);
+        }
+
+        // we want to trigger this useEffect code block when weatherData or removedCity changed.
+    }, [weatherData, removedCity])
+
+
+    useEffect(() => {
+        // when page is opened, just load previous search list for one time 
+        loadPreviousSearches();
+
+        return () => {
+            // like componentUnmount event in class component, we want to empty the previous search list when user refreshes the page. 
+            setPreviousSearches([]);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!isLoading) {
+            inputRef.current.select();
+            // loadPreviousSearches();
+        }
+
+    }, [isLoading])
+
+
+    useEffect(() => {
+        // if we have no weatherData, then do nothing.
+        if (!weatherData) {
+            return
+        }
+
+        // we want to run this code block when everytime weatherData changes.
+        // we just store the city name of lastly searched weather data.
+        // then get the removed city from the localStorage, if we reached 3 items in previous city names in localStorage.
+        // then set this city name to use further operations above for updating the recent weather data array (previousSearches)
+        const removedPreviousSearch = storeLocation(weatherData.name);
+        setRemovedCity(removedPreviousSearch);
+    }, [weatherData])
 
 
     return <div className="container">
         <div className="column">
             <form onSubmit={handleSearchSubmit} className="form">
-                <input value={location} onChange={(e) => setLocation(e.target.value)} className="form-input" />
+                <input value={location} ref={inputRef} onChange={(e) => setLocation(e.target.value)} className="form-input" />
                 <button className="form-btn">Search</button>
             </form>
             <div className="recent-searches-container">
@@ -77,7 +133,6 @@ const Home = () => {
                     <h1 className="recent-searches">Recent Searches</h1>
                     {
                         previousSearches.map((item, index) => {
-
                             return (
                                 <div className="recent-weather-column" key={index}>
                                     <h1 className="recent-country-name">{item.name}</h1>
@@ -90,15 +145,15 @@ const Home = () => {
                 </div>
             </div>
         </div>
-        <div className="current-weather-container">
-            <h1 className="current-city-title">{weatherData.name}</h1>
-            <img className="current-weather-img" src={`http://openweathermap.org/img/wn/${weatherData.weather ? weatherData.weather[0].icon : null}@2x.png`} alt={weatherData.name} />
-            <h2 className="current-temp">{weatherData.main ? weatherData.main.temp : ""} °C</h2>
-            <span className="current-details" onClick={() => navigate(
-                `${weatherData.name}`
-                // `https://api.openweathermap.org/data/2.5/weather?id=${weatherData.id}&dt=1643803200&appid=${API_KEY}`
-            )}>Details <img src={PlusCircle} alt="PlusCircle" /> </span>
-        </div>
+
+        {weatherData &&
+            <CurrentWeather
+                name={weatherData.name}
+                icon={weatherData.weather
+                    ? weatherData.weather[0].icon
+                    : null}
+                temp={weatherData.main ? weatherData.main.temp : ""} />}
+
 
     </div>
 }
