33cea26edc056e79b4b2827c813a7be87aeacfe1
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-09 20:15:18 +0300

    Use context api

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,16 +1,23 @@
 import PlusCircle from '../assets/images/plus-circle-svgrepo-com.svg';
+import { useContext } from 'react';
+import DataContext from '../Contexts/DataContext';
 import { useNavigate } from "react-router-dom";
 
-const CurrentWeather = ({ name, icon, temp }) => {
+const CurrentWeather = () => {
+    const { weatherData } = useContext(DataContext);
     const navigate = useNavigate();
 
     return <div className="current-weather-container">
-        <h1 className="current-city-title">{name}</h1>
-        <img className="current-weather-img" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={name} />
-        <h2 className="current-temp">{temp} °C</h2>
+        <h1 className="current-city-title">{weatherData.name}</h1>
+        <img className="current-weather-img" src={`http://openweathermap.org/img/wn/${weatherData.weather
+            ? weatherData.weather[0].icon
+            : null}@2x.png`} alt={weatherData.name} />
+        <h2 className="current-temp">{weatherData.main
+            ? weatherData.main.temp
+            : ""} °C</h2>
         <span className="current-details" onClick={() => navigate(
-            `${name}`
-        )}>Details <img src={PlusCircle} alt="PlusCircle" /> </span>
+            `${weatherData.name}`
+        )}>Details <img className='detail-plus-circle' src={PlusCircle} alt="PlusCircle" /> </span>
     </div>
 }
 
