cf363af14aee11bd6886e9fda2bbe070b7ee9637
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-09-11 18:26:50 +0300

    Add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
-  BrowserRouter,
   Routes,
-  Route
+  Route,
+  HashRouter
 } from "react-router-dom";
 
 import Login from '../src/Pages/Login';
@@ -9,19 +9,22 @@ import Home from './components/Home';
 import '../src/styles/main.scss';
 import Details from "./Pages/Details";
 import { DataProvider } from "../src/Contexts/DataContext";
+import { ThemeProvider } from "./Contexts/ThemeContext";
 
 function App() {
   return (
     <div>
-      <DataProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
-            <Route path=":name" element={<div className="app"><Details /></div>} />
-          </Routes>
-        </BrowserRouter>
-      </DataProvider>
+      <ThemeProvider>
+        <DataProvider>
+          <HashRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<Home />} />
+              <Route path=":name" element={<div className="app"><Details /></div>} />
+            </Routes>
+          </HashRouter>
+        </DataProvider>
+      </ThemeProvider>
     </div>
   );
 }
